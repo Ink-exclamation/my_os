316278078311b814330bf9d42a8f8a7e1879fe4a
Ink-exclamation <ink.atramento@outlook.jp>
2026-05-16 00:39:13 +0900

    Catch double faults

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,12 @@ pub extern "C" fn _start() -> ! {
 
 	my_os::init();
 
-	x86_64::instructions::interrupts::int3();
+	fn stack_overflow() {
+		stack_overflow();
+	}
+
+	stack_overflow();
+
 
 	#[cfg(test)]
 	test_main();
